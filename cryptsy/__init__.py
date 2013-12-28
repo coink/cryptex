232cf5a9d14280eb93ba78ca440aa1a5f6823aaf
Chris LaRose <cjlarose@gmail.com>
2013-12-28 05:04:47 -0700

    Added authenticated requests

--- cryptsy/__init__.py
+++ cryptsy/__init__.py
@@ -4,6 +4,8 @@ from hashlib import sha512
 from urllib import urlencode
 import requests
 
+API_ENDPOINT = 'https://www.cryptsy.com/api'
+
 class Cryptsy:
     def __init__(self, key, secret):
         self.key = key
@@ -26,19 +28,18 @@ class Cryptsy:
 
     def _perform_request(self, method, data={}):
         payload, headers = self.get_request_params(method, data)
-        r = requests.post('https://www.cryptsy.com/api', data=payload, headers=headers)
-        print r.headers
+        r = requests.post(ENDPOINT, data=payload, headers=headers)
         content = r.json()
         return content['return']
         
     def get_info(self):
-        pass
+        return self._perform_request('getinfo')
 
     def get_markets(self):
         return self._perform_request('getmarkets')
 
-    def get_my_transaction(self):
-        pass
+    def get_my_transactions(self):
+        return self._perform_request('mytransactions')
 
     def get_trades(self, market_id):
         return self._perform_request('markettrades', {'marketid': market_id}) 
@@ -50,31 +51,46 @@ class Cryptsy:
         return self._perform_request('mytrades', {'marketid': market_id, 'limit': limit})
 
     def get_all_my_trades(self):
-        pass
+        return self._perform_request('allmytrades')
 
     def get_my_orders(self, market_id):
-        pass
-
-    def get_depth(self, market_id):
-        pass
+        return self._perform_request('myorders', {'marketid': market_id})
 
     def get_all_my_orders(self):
-        pass
+        return self._perform_request('allmyorders')
+
+    def get_depth(self, market_id):
+        return self._perform_request('depth', {'marketid': market_id})
 
     def create_order(self, market_id, order_type, quantity, price):
-        pass
+        params = {
+            'marketid': market_id,
+            'ordertype', order_type,
+            'quantity': quantity,
+            'price', price
+        }
+        return self._perform_request('myorders', params)
 
     def cancel_order(self, order_id):
-        pass
+        return self._perform_request('cancelorder', {'orderid': order_id})
 
     def cancel_market_orders(self, market_id):
-        pass
+        return self._perform_request('cancelmarketorders', {'marketid': market_id})
 
     def cancel_all_orders(self):
-        pass
+        return self._perform_request('cancelallorders')
 
     def calculate_fees(self, order_type, quantity, price):
-        pass
+        params = {
+            'ordertype', order_type,
+            'quantity': quantity,
+            'price', price
+        }
+        return self._perform_request('calculatefees', params)
 
     def generate_new_address(self, currency):
-        pass
+        if isinstance(currency, int):
+            params = {'currencyid': currency}
+        else:
+            params = {'currencycode': currency}
+        return self._perform_request('generatenewaddress', params)
