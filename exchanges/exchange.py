cae4db9dbaa288da52f0381fd2231e717d8ab327
Chris LaRose <cjlarose@gmail.com>
2014-01-01 14:50:35 -0700

    Added descrptions of Exchange methods

--- exchanges/exchange.py
+++ exchanges/exchange.py
@@ -1,11 +1,23 @@
 class Exchange(object):
     def get_my_open_orders(self):
+        """
+        Returns a list of exchanges.order.Order that represent currently 
+        unfulfilled trade orders.
+        """
         raise NotImplementedError
 
     def get_my_trades(self):
+        """
+        Returns a list of exchanges.trade.Trade that represent all the user's 
+        trades.
+        """
         raise NotImplementedError
 
     def cancel_order(self, order_id):
+        """
+        Given an order_id, cancels the order associeted with the id. Returns 
+        None.
+        """
         raise NotImplementedError
     
     def buy(self, frm, to, quantity, price):
