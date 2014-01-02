16714cf284573d2c1612245a8893ab44b0c79c47
Chris LaRose <cjlarose@gmail.com>
2014-01-02 00:21:34 -0700

    Remove extra cryptsy methods

--- cryptex/cryptsy.py
+++ cryptex/cryptsy.py
@@ -26,7 +26,7 @@ class Cryptsy(Exchange, SignedSingleEndpoint):
         if self.timezone is not None:
             return self.timezone
 
-        info = self.get_info()
+        info = self._get_info()
         return pytz.timezone(info['servertimezone'])
 
     def _convert_timestamp(self, time_str):
@@ -44,7 +44,7 @@ class Cryptsy(Exchange, SignedSingleEndpoint):
         Given a market_id, this function returns a two-tuple containing the currencies involved.
         """
         if self.market_currency_map is None:
-            markets = self.get_markets()
+            markets = self.perform_request('getmarkets')
             self.market_currency_map = {
                 m['marketid']:
                 (m['primary_currency_code'], m['secondary_currency_code'])
@@ -52,7 +52,7 @@ class Cryptsy(Exchange, SignedSingleEndpoint):
             }
         return self.market_currency_map[market_id]
 
-    def get_info(self):
+    def _get_info(self):
         return self.perform_request('getinfo')
 
     def get_markets(self):
@@ -61,18 +61,6 @@ class Cryptsy(Exchange, SignedSingleEndpoint):
             (m['primary_currency_code'], m['secondary_currency_code']) 
             for m in markets
         ]
-
-    def get_my_transactions(self):
-        return self.perform_request('mytransactions')
-
-    def get_trades(self, market_id):
-        return self.perform_request('markettrades', {'marketid': market_id}) 
-
-    def get_orders(self, market_id):
-        return self.perform_request('marketorders', {'marketid': market_id}) 
-
-    #def get_my_trades(self, market_id, limit=200):
-    #    return self.perform_request('mytrades', {'marketid': market_id, 'limit': limit})
 
     def _format_trade(self, trade):
         if trade['tradetype'] == 'Buy':
@@ -98,9 +86,6 @@ class Cryptsy(Exchange, SignedSingleEndpoint):
         trades = self.perform_request('allmytrades')
         return [self._format_trade(t) for t in trades]
 
-    def get_my_orders(self, market_id):
-        return self.perform_request('myorders', {'marketid': market_id})
-
     def _format_order(self, order):
         if order['ordertype'] == 'Buy':
             order_type = Trade.BUY
@@ -123,39 +108,6 @@ class Cryptsy(Exchange, SignedSingleEndpoint):
         orders = self.perform_request('allmyorders')
         return [self._format_order(o) for o in orders]
 
-    def get_depth(self, market_id):
-        return self.perform_request('depth', {'marketid': market_id})
-
-    def create_order(self, market_id, order_type, quantity, price):
-        params = {
-            'marketid': market_id,
-            'ordertype': order_type,
-            'quantity': quantity,
-            'price': price
-        }
-        return self.perform_request('myorders', params)
-
     def cancel_order(self, order_id):
         self.perform_request('cancelorder', {'orderid': order_id})
         return None
-
-    def cancel_market_orders(self, market_id):
-        return self.perform_request('cancelmarketorders', {'marketid': market_id})
-
-    def cancel_all_orders(self):
-        return self.perform_request('cancelallorders')
-
-    def calculate_fees(self, order_type, quantity, price):
-        params = {
-            'ordertype': order_type,
-            'quantity': quantity,
-            'price': price
-        }
-        return self.perform_request('calculatefees', params)
-
-    def generate_new_address(self, currency):
-        if isinstance(currency, int):
-            params = {'currencyid': currency}
-        else:
-            params = {'currencycode': currency}
-        return self.perform_request('generatenewaddress', params)
