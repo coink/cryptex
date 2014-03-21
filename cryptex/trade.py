coink/cryptex

commit db9b40aa45dcdf8f891c3bb4b0a524ac245b5a24

Adapt transaction, trade and order objects. Add a common file and a common quantize method Fix file indentation
Conflicts:
	cryptex/exchange/btce.py
	cryptex/exchange/cryptsy.py
	cryptex/trade.py
	cryptex/transaction.py

cryptex/trade.py
@@ -1,21 +1,65 @@
+import cryptex.common as common
+
 class Trade(object):
-    BUY = 0
-    SELL = 1
-    def __init__(self, trade_id, trade_type, base_currency, counter_currency,
-        time, order_id, amount, price, fee=None):
-        self.trade_id = trade_id
-        self.trade_type = trade_type
+    '''
+    Basic trade
+    '''
+    trade_type = 0
+    def __init__(self, trade_id, base_currency, counter_currency,
+                datetime, order_id, amount, price, fee=None, fee_currency=None):
+        ''''
+        :param trade_id: reference id on the exchange
+        :param base_currency: the base currency of the trade (LTC in "LTC/BTC")
+        :param counter_currency: the counter currency of the trade (BTC in "LTC/BTC")
+        :param datetime: date and time of the trade in UTC
+        :param order_id: id of referencing order on the exchange
+        :param amount: amount that was bought/sold
+        :param price: price at which the trade was made
+        :param fee: anmount of fee payed to the exchange
+        :param fee_currency: the currency the fee was payed in (base_currency or counter_currency)
+        '''
         self.base_currency = base_currency
         self.counter_currency = counter_currency
-        self.time = time
+        self.datetime = datetime
         self.order_id = order_id
         self.amount = amount
         self.price = price
         self.fee = fee
+        self.fee_currency = fee_currency
+
+    def __setattr__(self, name, value):
+        if name == 'fee_currency' and self.fee and \
+                value not in (self.base_currency, self.counter_currency):
+            raise ValueError('Wrong fee_currency "%r"' % value)
+        super(Trade, self).__setattr__(name, value)
+
+    def type(self):
+        return self.__class__.__name__
 
     def __str__(self):
-        if self.trade_type == Trade.BUY:
-            ts = 'Buy'
-        else:
-            ts = 'Sell'
-        return '<%s of %.8f %s>' % (ts, self.amount, self.base_currency)
+        return '<%s of %.8f %s>' % (self.type(),
+                                    self.amount,
+                                    self.base_currency)
+
+
+class Buy(Trade):
+    trade_type = 1
+
+    def netto_amount(self):
+        if self.fee is not None:
+            return common.quantize(self.amount - self.fee)
+        return self.amount
+
+    def netto_total(self):
+        return common.quantize(self.amount * self.price)
+
+class Sell(Trade):
+    trade_type = 2
+
+    def netto_amount(self):
+        return self.amount
+
+    def netto_total(self):
+        if self.fee is not None:
+            return common.quantize((self.amount * self.price) - self.fee)
+        return common.quantize(self.amount * self.price)

cryptex/common.py
@@ -1,0 +1,11 @@
+import decimal
+
+decimal.DefaultContext.rounding = decimal.ROUND_DOWN
+decimal.DefaultContext.traps = decimal.ExtendedContext.traps.copy()
+decimal.DefaultContext.traps[decimal.InvalidOperation] = 1
+decimal.setcontext(decimal.DefaultContext)
+
+DECIMAL_PRECISION = decimal.Decimal(10) ** -8
+
+def quantize(decimal_value):
+	return decimal_value.quantize(DECIMAL_PRECISION)

cryptex/exchange/btce.py
@@ -1,10 +1,9 @@
 import datetime
-
 import pytz
 
 from cryptex.exchange import Exchange
-from cryptex.trade import Trade
-from cryptex.order import Order
+from cryptex.trade import Sell, Buy
+from cryptex.order import SellOrder, BuyOrder
 from cryptex.transaction import Transaction, Deposit, Withdrawal
 from cryptex.exchange.single_endpoint import SingleEndpoint, SignedSingleEndpoint
 from cryptex.exception import APIException
@@ -113,45 +112,41 @@ class BTCE(BTCEBase, Exchange, SignedSingleEndpoint):
             else:
                 raise e
 
-    @staticmethod
-    def _format_trade(trade_id, trade):
-        if trade['type'] == 'buy':
-            trade_type = Trade.BUY
-        else:
-            trade_type = Trade.SELL
-
+    def _format_trade(self, trade_id, trade):
         base, counter = BTCE._pair_to_market(trade['pair'])
+        if trade['type'] == 'buy':
+            trade_type = Buy
+        else:
+            trade_type = Sell
 
-        return Trade(
+        return trade_type(
             trade_id = trade_id,
-            trade_type = trade_type,
             base_currency = base.upper(),
             counter_currency = counter.upper(),
-            time = BTCE._format_timestamp(trade['timestamp']),
+            datetime = BTCE._format_timestamp(trade['timestamp']),
             order_id = trade['order_id'],
             amount = trade['amount'],
-            price = trade['rate']
+            price = trade['rate'],
         )
 
     def get_my_trades(self):
         trades = self.perform_request('TradeHistory')
-        return [BTCE._format_trade(t_id, t) for t_id, t in trades.iteritems()]
+        return [self._format_trade(t_id, t) for t_id, t in trades.iteritems()]
 
     @staticmethod
     def _format_order(order_id, order):
         if order['type'] == 'buy':
-            order_type = Trade.BUY
+            order_type = BuyOrder
         else:
-            order_type = Trade.SELL
+            order_type = SellOrder
 
         base, counter = BTCE._pair_to_market(order['pair'])
 
-        return Order(
+        return order_type(
             order_id = order_id,
-            order_type = order_type,
             base_currency = base.upper(),
             counter_currency = counter.upper(),
-            time = BTCE._format_timestamp(order['timestamp_created']),
+            datetime = BTCE._format_timestamp(order['timestamp_created']),
             amount = order['amount'],
             price = order['rate']
         )

cryptex/exchange/cryptsy.py
@@ -1,12 +1,12 @@
 import datetime
 from decimal import Decimal, InvalidOperation
-
 import pytz
 
+import cryptex.common as common
 from cryptex.exception import CryptsyException
 from cryptex.exchange import Exchange
-from cryptex.trade import Trade
-from cryptex.order import Order
+from cryptex.trade import Sell, Buy
+from cryptex.order import SellOrder, BuyOrder
 from cryptex.transaction import Transaction, Deposit, Withdrawal
 from cryptex.exchange.single_endpoint import SingleEndpoint, SignedSingleEndpoint
 
@@ -134,42 +134,51 @@ class Cryptsy(CryptsyBase, Exchange, SignedSingleEndpoint):
 
     def _format_trade(self, trade):
         if trade['tradetype'] == 'Buy':
-            trade_type = Trade.BUY
+            trade_type = Buy
         else:
-            trade_type = Trade.SELL
+            trade_type = Sell
 
         base, counter = self._get_currencies(trade['marketid'])
 
-        return Trade(
+        return trade_type(
             trade_id = trade['tradeid'],
-            trade_type = trade_type,
             base_currency = base,
             counter_currency = counter,
-            time = self._convert_datetime(trade['datetime']),
+            datetime = self._convert_datetime(trade['datetime']),
             order_id = trade['order_id'],
             amount = Decimal(trade['quantity']),
             price = Decimal(trade['tradeprice']),
             fee = Decimal(trade['fee'])
+            # Cryptsy's fee is always taken from counter_currency
+            fee_currency = counter,
         )
 
-    def get_my_trades(self):
-        trades = self.perform_request('allmytrades')
+    def get_my_trades(self, limit=200, market=None):
+        params = {'limit': limit}
+        if market is None:
+            trades = self.perform_request('allmytrades', params)
+        else:
+            params['marketid'] = self._get_market_id(market)
+            trades = self.perform_request('mytrades', params)
+            for index, trade in enumerate(trades):
+                trade['marketid'] = params['marketid']
+                trades[index] = trade
         return [self._format_trade(t) for t in trades]
 
     def _format_order(self, order):
         if order['ordertype'] == 'Buy':
-            order_type = Trade.BUY
+            order_type = BuyOrder
         else:
-            order_type = Trade.SELL
+            order_type = SellOrder
 
         base, counter = self._get_currencies(order['marketid'])
 
-        return Order(
+        return order_type(
             order_id = order['orderid'],
             order_type = order_type,
             base_currency = base,
             counter_currency = counter,
-            time = self._convert_datetime(order['created']),
+            datetime = self._convert_datetime(order['created']),
             amount = Decimal(order['quantity']),
             price = Decimal(order['price'])
         )

cryptex/order.py
@@ -1,13 +1,25 @@
 class Order(object):
-    def __init__(self, order_id, order_type, base_currency, counter_currency,
-        time, amount, price):
+    '''
+    Basic order
+    '''
+    order_type = 0
+    def __init__(self, order_id, base_currency, counter_currency,
+                datetime, amount, price):
         self.order_id = order_id
         self.order_type = order_type
         self.base_currency = base_currency
         self.counter_currency = counter_currency
-        self.time = time
+        self.datetime = datetime
         self.amount = amount
         self.price = price
 
+    def type(self):
+        return self.__class__.__name__
+
     def __str__(self):
         return repr(self.__dict__)
+
+class BuyOrder(Order):
+    order_type = 1
+class SellOrder(Order):
+    order_type = 2

cryptex/transaction.py
@@ -1,22 +1,25 @@
 class Transaction(object):
-	''' Transaction that is neither deopsit nor withdrawal
-	Used for CryptsyPoint credit
-	'''
-	transaction_type = 0
-	def __init__(self, transaction_id, datetime, currency, amount, address, fee=None):
-		self.transaction_id = transaction_id
-		self.datetime = datetime
-		self.currency = currency
-		self.amount = amount
-		self.address = address
-		self.fee = fee
+    '''
+    Transaction that is neither deopsit nor withdrawal
+    Used for CryptsyPoint credit
+    '''
+    transaction_type = 0
+    def __init__(self, transaction_id, datetime, currency, amount, address, fee=None):
+        self.transaction_id = transaction_id
+        self.datetime = datetime
+        self.currency = currency
+        self.amount = amount
+        self.address = address
+        self.fee = fee
+    
+    def type(self):
+        return self.__class__.__name__
 
-	def __str__(self):
-		return '<%s transaction of %.8f %s>' % (self.__class__.__name__,
-												self.amount,
-												self.currency)
+    def __str__(self):
+        return '<%s transaction of %.8f %s>' % (self.type(),
+                                                self.amount,
+                                                self.currency)
 class Deposit(Transaction):
-	transaction_type = 1
-
-class Withdrawal(Transaction):
-	transaction_type = 2
+    transaction_type = 1
+class Withdraw(Transaction):
+    transaction_type = 2

commit 3cdf89524171cb44d399095edb04eddf3f7a71d6

db9b40a accidentally removed the self.trade_id assignment

cryptex/trade.py
@@ -18,6 +18,7 @@ class Trade(object):
         :param fee: anmount of fee payed to the exchange
         :param fee_currency: the currency the fee was payed in (base_currency or counter_currency)
         '''
+        self.trade_id = trade_id
         self.base_currency = base_currency
         self.counter_currency = counter_currency
         self.datetime = datetime