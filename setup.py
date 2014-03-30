1340682f4b85b05b7dde755bf71b04bb5eafd583
Chris LaRose <cjlarose@gmail.com>
2014-03-30 13:37:37 -0700

    Add missing package to setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version='0.1',
     author='Chris LaRose',
     author_email='cjlarose@gmail.com',
-    packages=['cryptex'],
+    packages=['cryptex', 'cryptex.exchange'],
     install_requires=[
         'requests>=2.1.0,<3.0',
         'pytz>=2013.8'
