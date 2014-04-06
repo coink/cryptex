fb2bacf5cd01d4ca0ecb4f3002e062a46df0cf67
sjdev <shanejgianelli@gmail.com>
2014-04-06 12:06:13 -0700

    Blockchain.info stream wrapper works

---
+++ cryptex/soxex/blockchaininfo.py
@@ -0,0 +1,30 @@
+from cryptex.soxex.wsconsumer import WebSocketConsumer
+from cryptex.soxex.socketserver import WebSocketBase
+
+from pprint import pprint
+
+class BlockChainInfoSocket(WebSocketBase, WebSocketConsumer):
+    WEBSOCKET_HOST = 'ws://ws.blockchain.info/inv'
+
+    def __init__(self):
+        super(BlockChainInfoSocket, self).__init__()
+
+    def connect(self):
+        self.start_socket(self.WEBSOCKET_HOST)
+
+    def subscribe_txs(self):
+        if self.socket is None:
+            self.connect()
+            self.message_callback = self.on_message
+
+        self.send_message('{"op":"unconfirmed_sub"}')
+
+    def subscribe_address(self, addr):
+        if self.socket is None:
+            self.connect()
+            self.message_callback = self.on_message
+
+        self.send_message('{"op":"addr_sub", "addr":"%s"}' % addr)
+
+    def on_message(self, data):
+        pprint(data)
