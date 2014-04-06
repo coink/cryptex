f6762518a94d001dd2451757f831e31481a48901
sjdev <shanejgianelli@gmail.com>
2014-04-06 14:15:21 -0700

    Refactored the Pusher interface so that it can handel multiple simultaneous connections

--- cryptex/soxex/pushersocket.py
+++ cryptex/soxex/pushersocket.py
@@ -4,25 +4,47 @@ import logging
 
 from pprint import pprint
 
-class PusherClientInterface(object):
+class PusherConnection(object):
 
-    def open_connection(self, key, channel, event, callback):
-        self.pusher_channel = channel
-        self.pusher_event = event
-        self.pusher_key = key
-        self.pusher_event = event
-        self.pusher_callback = callback
+    def __init__(self, key, channel, event, callback):
+        self.channel_name = channel
+        self.event = event
+        self.key = key
+        self.event = event
+        self.callback = callback
 
         def json_proxy(data):
-            self.pusher_callback(json.loads(data))
+            self.callback(json.loads(data))
 
         def handle_connect(data):
-            channel = socket.subscribe(self.pusher_channel)
-            channel.bind(self.pusher_event, json_proxy)
+            channel = self.socket.subscribe(self.channel_name)
+            channel.bind(self.event, json_proxy)
 
-        socket = pusher.Pusher(key, log_level=logging.ERROR)
-        socket.connection.bind('pusher:connection_established', handle_connect)
-        socket.connect()
+        self.socket = pusher.Pusher(key, log_level=logging.ERROR)
+        self.socket.connection.bind('pusher:connection_established', handle_connect)
+        self.socket.connect()
 
-    def close_connection(self):
+    def disconnect(self):
         self.socket.disconnect()
+
+class PusherClientInterface(object):
+
+    def __init__(self):
+        super(PusherClientInterface, self).__init__()
+        self.channels = {}
+
+    def connect(self, key, channel, event, callback):
+        if channel in self.channels:
+            self.close(channel)
+
+        self.channels[channel] = PusherConnection(key, channel, event, callback)
+
+    def close(self, channel=None):
+        if channel is None:
+            for channel in self.channels.values():
+                channel.disconnect()
+
+            self.channels = {}
+        else:
+            self.channels[channel].disconnect()
+            self.channels.pop(channel)
