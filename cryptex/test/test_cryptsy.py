ee21a96e936f45e931fd47fcc63c7ed158d4979a
Chris LaRose <cjlarose@gmail.com>
2014-04-13 13:44:08 -0700

    Mock tests support mocking many requests

--- cryptex/test/test_cryptsy.py
+++ cryptex/test/test_cryptsy.py
@@ -3,20 +3,31 @@ import io
 import unittest
 import httpretty
 import requests
+from cryptex.exchange import Cryptsy
 
 test_dir = os.path.dirname(os.path.realpath(__file__))
 mock_dir = os.path.join(test_dir, 'mocks')
 
 class CryptsyMock():
-    def __init__(self, filename):
-        self.filename = filename
+    """
+    Responses should be a {method: filename} map
+    """
+    def __init__(self, responses):
+        self.responses = responses
+
+    def request_callback(self, request, uri, headers):
+        print request
+        print request.__dict__
+        method = request.parsed_body[u'method'][0]
+        filename = self.responses[method]
+        with io.open(os.path.join(mock_dir, filename), 'r') as f:
+            contents = f.read()
+        return (200, headers, contents)
 
     def __enter__(self):
         httpretty.enable()
-        with io.open(os.path.join(mock_dir, self.filename), 'r') as f:
-            contents = f.read()
-            httpretty.register_uri(httpretty.GET, "https://api.cryptsy.com/api",
-                                   body=contents)
+        httpretty.register_uri(httpretty.POST, "https://api.cryptsy.com/api",
+                               body=self.request_callback)
 
     def __exit__(self, type, value, traceback):
         httpretty.disable()
@@ -32,10 +43,9 @@ class TestCryptsyPrivate(unittest.TestCase):
         pass
 
     def test_one(self):
-        with CryptsyMock('all_my_trades.json'):
-            response = requests.get('https://api.cryptsy.com/api')
-            content = response.json() 
-            self.assertEqual(content['success'], "1")
+        with CryptsyMock({'allmytrades': 'all_my_trades.json'}):
+            trade = Cryptsy('key', 'secret').get_my_trades()[0]
+            self.assertEqual(trade['tradeid'], u'27208199')
 
 if __name__ == '__main__':
     unittest.main()
