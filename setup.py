42abb1a14684c6649c06a6625c3d63bf8fc16b18
sjdev <shanejgianelli@gmail.com>
2014-04-13 11:18:24 -0700

    Fixed setup details for soxex

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ setup(
     version='0.1',
     author='Chris LaRose',
     author_email='cjlarose@gmail.com',
-    packages=['cryptex', 'cryptex.exchange'],
+    packages=['cryptex', 'cryptex.soxex'],
     install_requires=[
         'requests>=2.1.0,<3.0',
-        'pytz>=2013.8'
+        'pytz>=2013.8',
+        'websocket-client==0.12.0',
+        'pusherclient==0.2.0'
     ]
 )
