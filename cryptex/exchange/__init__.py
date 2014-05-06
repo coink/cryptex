dd6518ed4e1e2a718ae884e833302c8bfb28f0d1
sjdev <shanejgianelli@gmail.com>
2014-05-06 16:26:13 -0700

    Removed unneccessary imports

--- cryptex/exchange/__init__.py
+++ cryptex/exchange/__init__.py
@@ -1,3 +1,3 @@
 from cryptex.exchange.exchange import Exchange
-from cryptex.exchange.cryptsy import Cryptsy, CryptsyPublic
-from cryptex.exchange.btce import BTCE, BTCEPublic
+from cryptex.exchange.cryptsy import Cryptsy
+from cryptex.exchange.btce import BTCE
